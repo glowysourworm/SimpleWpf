cb2ad31beafb7d32c90fd833f3c365f002e267da
glowysourworm <aniv.sm.esports@gmail.com>
2025-06-21 11:58:14 -0500

    2 converters

---
+++ src/SimpleWpf.UI/Converter/TimeSpan/TimeSpanMillisecondsConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SimpleWpf.UI.Converter
+{
+    public class TimeSpanMillisecondsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+
+            var timeSpan = (TimeSpan)value;
+
+            return timeSpan.TotalMilliseconds;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+
+            var milliseconds = (int)value;
+
+            return TimeSpan.FromMilliseconds(milliseconds);
+        }
+    }
+}
---
+++ src/SimpleWpf.UI/Converter/TimeSpan/TimeSpanRatioParameterConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SimpleWpf.UI.Converter
+{
+    public class TimeSpanRatioParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return Binding.DoNothing;
+
+            var timeSpan = (TimeSpan)value;
+            var totalTime = (TimeSpan)parameter;
+
+            return timeSpan.TotalMilliseconds / (float)totalTime.TotalMilliseconds;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return Binding.DoNothing;
+
+            var ratio = (float)value;
+            var totalTime = (TimeSpan)parameter;
+
+            return TimeSpan.FromMilliseconds(totalTime.TotalMilliseconds * ratio);
+        }
+    }
+}
