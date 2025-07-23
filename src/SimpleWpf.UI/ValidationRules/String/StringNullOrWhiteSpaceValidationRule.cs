75a3274815570fc6910b74a0931498fa5941cf0e
glowysourworm <aniv.sm.esports@gmail.com>
2025-07-23 03:58:54 -0500

    Added validation rule base

---
+++ src/SimpleWpf.UI/ValidationRules/String/StringNullOrWhiteSpaceValidationRule.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleWpf.UI.ValidationRules
+{
+    public class StringNullOrWhiteSpaceValidationRule : ValidationRuleBase
+    {
+        string _valueName;
+
+        public string ValueName
+        {
+            get { return _valueName; }
+            set { this.RaiseAndSetIfChanged(ref _valueName, value); }
+        }
+
+
+        public StringNullOrWhiteSpaceValidationRule() 
+        {
+            ValueName = "Value";
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrWhiteSpace((value as string)))
+                return new ValidationResult(false, this.ValueName + " must not be empty or white space");
+
+            else
+                return ValidationResult.ValidResult;
+        }
+    }
+}
