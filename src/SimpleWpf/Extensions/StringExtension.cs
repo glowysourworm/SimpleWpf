a63ce8eed5890e482b884808675ec445f0b19961
glowysourworm <ryanddolan@hotmail.com>
2026-08-15 15:26:21 -0500

    some string trimming functions

--- src/SimpleWpf/Extensions/StringExtension.cs
+++ src/SimpleWpf/Extensions/StringExtension.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleWpf.Extensions
+﻿namespace SimpleWpf.Extensions
 {
     public static class StringExtension
     {
@@ -17,6 +11,151 @@ namespace SimpleWpf.Extensions
                 return str;
 
             return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+
+        /// <summary>
+        /// Left aligned string limiting function with options for ellipses
+        /// </summary>
+        public static string LimitLengthLeft(this string str, uint length = uint.MaxValue, bool useEllipses = true)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            if (str.Length > length)
+            {
+                if (!useEllipses)
+                    return str.Substring(0, (int)length);
+
+                else
+                {
+                    if (length > 3)
+                        return str.Substring(0, (int)length - 3) + "...";
+
+                    else
+                        return str.Substring(0, (int)length);
+                }
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Right-aligned string limiting function with option for ellipses
+        /// </summary>
+        public static string LimitLengthRight(this string str, uint length = uint.MaxValue, bool useEllipses = true)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            if (str.Length > length)
+            {
+                if (!useEllipses)
+                    return str.Substring(str.Length - (int)length, (int)length);
+
+                else
+                {
+                    if (str.Length - length > 3)
+                        return "..." + str.Substring(str.Length - (int)length + 3, (int)length - 3);
+
+                    else
+                        return str.Substring(str.Length - (int)length, (int)length);
+                }
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Right aligns string, padding to the left, and ellipses option
+        /// </summary>
+        public static string ForceLengthRight(this string str, uint length = 10, bool useEllipses = true)
+        {
+            if (length > 10000)
+                throw new ArgumentException("Trying to force string length to very large number. Please use local method. (or limit to 10000 chars)");
+
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            // Limit
+            if (str.Length > length)
+                return str.LimitLengthRight(length, useEllipses);
+
+            // Pad
+            else
+            {
+                var result = string.Empty;
+                var canUseEllipses = str.Length - length >= 3;
+
+
+
+                // Padding chars
+                for (int index = 0; index < length; index++)
+                {
+                    // Ellipses
+                    if (useEllipses && canUseEllipses)
+                    {
+                        if (index < length - str.Length - 3)
+                            result += ' ';
+
+                        else if (index < length - str.Length)
+                            result += '.';
+
+                        else
+                            result += str[index - ((int)length - str.Length)];
+                    }
+
+                    // No Ellipses
+                    else
+                    {
+                        if (index < length - str.Length)
+                            result += ' ';
+
+                        else
+                            result += str[index - ((int)length - str.Length)];
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Right aligns padded string, with option for ellipses
+        /// </summary>
+        public static string ForceLengthLeft(this string str, uint length = 10, bool useEllipses = false)
+        {
+            if (length > 10000)
+                throw new ArgumentException("Trying to force string length to very large number. Please use local method. (or limit to 10000 chars)");
+
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            // Limit
+            if (str.Length > length)
+                return str.LimitLengthLeft(length, useEllipses);
+
+            // Pad
+            else
+            {
+                var result = string.Empty;
+                var canUseEllipses = str.Length - length >= 3;
+
+                // Padding chars
+                for (int index = 0; index < length; index++)
+                {
+                    if (index < str.Length)
+                        result += str[index];
+
+                    else if (index < str.Length + 3 && useEllipses && canUseEllipses)
+                        result += '.';
+
+                    else
+                        result += ' ';
+                }
+
+                return result;
+
+            }
         }
 
         /// <summary>
@@ -79,7 +218,7 @@ namespace SimpleWpf.Extensions
                 }
             }
 
-            return theString; 
+            return theString;
         }
     }
 }
